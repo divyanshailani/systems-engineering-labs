daab374caab811a947b797f856716a4c3acb14a8
divyanshailani <ailanidivyansh@gmail.com>
2026-05-12 20:13:11 +0530

    Add Station 11 and 12 solutions

--- sql/hackerrank_basic_select.sql
+++ sql/hackerrank_basic_select.sql
@@ -27,3 +27,25 @@ LIMIT 1;
 SELECT DISTINCT city 
 FROM station 
 WHERE LEFT(city, 1) IN ('a', 'e', 'i', 'o', 'u');
+
+---------------------------------------------------------
+
+-- Problem: Weather Observation Station 11
+-- Query the list of CITY names from STATION that either do not start with vowels or do not end with vowels. 
+-- Your result cannot contain duplicates.
+
+SELECT DISTINCT city 
+FROM station 
+WHERE LEFT(city, 1) NOT IN ('a', 'e', 'i', 'o', 'u')
+   OR RIGHT(city, 1) NOT IN ('a', 'e', 'i', 'o', 'u');
+
+---------------------------------------------------------
+
+-- Problem: Weather Observation Station 12
+-- Query the list of CITY names from STATION that do not start with vowels and do not end with vowels. 
+-- Your result cannot contain duplicates.
+
+SELECT DISTINCT city 
+FROM station 
+WHERE LEFT(city, 1) NOT IN ('a', 'e', 'i', 'o', 'u')
+   AND RIGHT(city, 1) NOT IN ('a', 'e', 'i', 'o', 'u');
