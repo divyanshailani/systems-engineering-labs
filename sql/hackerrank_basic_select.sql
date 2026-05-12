b47a9025602bfe37a7c3b383bc098bb48f82e4aa
divyanshailani <ailanidivyansh@gmail.com>
2026-05-12 19:18:34 +0530

    Add Station 6 solution and rename file to hackerrank_basic_select.sql

--- sql/hackerrank_basic_select.sql
+++ sql/hackerrank_basic_select.sql
@@ -13,3 +13,17 @@ SELECT city, LENGTH(city)
 FROM station 
 ORDER BY LENGTH(city) DESC, city ASC 
 LIMIT 1;
+
+---------------------------------------------------------
+
+-- Problem: Weather Observation Station 6
+-- Query the list of CITY names starting with vowels (i.e., a, e, i, o, or u) from STATION. 
+-- Your result cannot contain duplicates.
+
+-- Approach 1: Using REGEXP (Concise)
+-- SELECT DISTINCT city FROM station WHERE city REGEXP '^[aeiou]';
+
+-- Approach 2: Using LEFT and IN (User Implemented)
+SELECT DISTINCT city 
+FROM station 
+WHERE LEFT(city, 1) IN ('a', 'e', 'i', 'o', 'u');
